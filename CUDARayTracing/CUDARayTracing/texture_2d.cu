387ec57f063f4d20bf15f181ca4e4511db9ad755
McThrok <wojtek.trze@gmail.com>
2019-10-22 19:16:18 +0200

    add structures test

--- CUDARayTracing/CUDARayTracing/texture_2d.cu
+++ CUDARayTracing/CUDARayTracing/texture_2d.cu
@@ -5,9 +5,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <DirectXMath.h>
+
 #define PI 3.1415926536f
 
-__global__ void cuda_kernel_texture_2d(unsigned char* surface, int width, int height, size_t pitch, float* spheres, int num_sphere)
+struct qwe {
+	int a;
+	int b;
+	DirectX::XMFLOAT3 c;
+
+	__device__ void Test() {
+		a++;
+	}
+};
+
+__global__ void cuda_kernel_texture_2d(qwe q,unsigned char* surface, int width, int height, size_t pitch, float* spheres, int num_sphere)
 {
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	int y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -22,6 +34,9 @@ __global__ void cuda_kernel_texture_2d(unsigned char* surface, int width, int he
 	pixel[1] = 0.0 * y / height; // green
 	pixel[2] = spheres[3]; // blue
 	pixel[3] = 1; // alpha
+
+	q.Test();
+	pixel[0] = q.c.x;
 }
 
 extern "C"
@@ -32,7 +47,12 @@ void cuda_texture_2d(void* surface, int width, int height, size_t pitch, float* 
 	dim3 Db = dim3(16, 16);   // block dimensions are fixed to be 256 threads
 	dim3 Dg = dim3((width + Db.x - 1) / Db.x, (height + Db.y - 1) / Db.y);
 
-	cuda_kernel_texture_2d << <Dg, Db >> > ((unsigned char*)surface, width, height, pitch, spheres, num_sphere);
+	qwe q;
+	q.a = 1;
+	q.b = 1;
+	q.c = { 1,0,1 };
+
+	cuda_kernel_texture_2d << <Dg, Db >> > (q, (unsigned char*)surface, width, height, pitch, spheres, num_sphere);
 
 	error = cudaGetLastError();
 
